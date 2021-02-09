d8853a1939457ae562fbeb32f943f6678ada6328
Daniel <P2533603@my365.dmu.ac.uk>
2021-02-09 13:38:06 +0000

    Added all the required unit tests and the associated C# code for Order Processing. Tests passed 9/9

--- ClassLibrary/clsOrders.cs
+++ ClassLibrary/clsOrders.cs
@@ -1,8 +1,17 @@
-﻿namespace ClassLibrary
+﻿using System;
+
+namespace ClassLibrary
 {
     public class clsOrders
     {
 
         public bool Active { get; set; }
+        public DateTime ExpectedDeliveryDate { get; set; }
+        public int OrderId { get; set; }
+        public string OrderContents { get; set; }
+        public bool DispatchedStatus { get; set; }
+        public double TotalPrice { get; set; }
+        public string DeliveryAddress { get; set; }
+        public bool PaymentConfirmed { get; set; }
     }
 }
--- Testing4/tsOrders.cs
+++ Testing4/tsOrders.cs
@@ -22,5 +22,69 @@ namespace Testing4
             AnOrder.Active = TestData;
             Assert.AreEqual(AnOrder.Active, TestData);
         }
+
+        [TestMethod]
+        public void OrderIdOK()
+        {
+            clsOrders AnOrder = new clsOrders();
+            Int32 TestData = 1;
+            AnOrder.OrderId = TestData;
+            Assert.AreEqual(AnOrder.OrderId, TestData);
+        }
+
+        [TestMethod]
+        public void OrderContentsOK()
+        {
+            clsOrders AnOrder = new clsOrders();
+            String TestData = "Some products in an order";
+            AnOrder.OrderContents = TestData;
+            Assert.AreEqual(AnOrder.OrderContents, TestData);
+        }
+
+        [TestMethod]
+        public void DeliveryDateAddedPropertyOK()
+        {
+            clsOrders AnOrder = new clsOrders();
+            DateTime TestData = DateTime.Now.Date;
+            AnOrder.ExpectedDeliveryDate = TestData;
+            Assert.AreEqual(AnOrder.ExpectedDeliveryDate, TestData);
+        }
+
+        [TestMethod]
+        public void DispatchedStatusOK()
+        {
+            clsOrders AnOrder = new clsOrders();
+            Boolean TestData = true;
+            AnOrder.DispatchedStatus = TestData;
+            Assert.AreEqual(AnOrder.DispatchedStatus, TestData);
+        }
+
+        [TestMethod]
+        public void TotalPriceOK()
+        {
+            clsOrders AnOrder = new clsOrders();
+            Double TestData = 12.32;
+            AnOrder.TotalPrice = TestData;
+            Assert.AreEqual(AnOrder.TotalPrice, TestData);
+        }
+
+        [TestMethod]
+        public void DeliveryAddressOK()
+        {
+            clsOrders AnOrder = new clsOrders();
+            String TestData = "A customer address";
+            AnOrder.DeliveryAddress = TestData;
+            Assert.AreEqual(AnOrder.DeliveryAddress, TestData);
+        }
+
+        [TestMethod]
+        public void PaymentConfirmedOK()
+        {
+            clsOrders AnOrder = new clsOrders();
+            Boolean TestData = true;
+            AnOrder.PaymentConfirmed = TestData;
+            Assert.AreEqual(AnOrder.PaymentConfirmed, TestData);
+        }
     }
+
 }

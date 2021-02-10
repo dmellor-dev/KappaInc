fc847926e1328b151d43c87a671e99cdef5ce817
Kacper Bziuk <kacper.bziuk.99@gmail.com>
2021-02-10 18:07:29 +0000

    Added initial testing and a class for the Staff management component

---
+++ Testing3/tstStaff.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using ClassLibrary;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Testing3
+{
+    [TestClass]
+    public class tstStaff
+    {
+        [TestMethod]
+        public void InstanceOK()
+        {
+            clsStaff StaffMember = new clsStaff();
+            Assert.IsNotNull(StaffMember);
+        }
+    }
+}

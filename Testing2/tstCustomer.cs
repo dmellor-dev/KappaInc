ae263651e3e0540b9db1d09f2042a3a4bedce3f6
Samuel Heelis <P2532413@my365.dmu.ac.uk>
2021-02-11 16:59:27 +0000

    added testing and class for Customer management

---
+++ Testing2/tstCustomer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using ClassLibrary;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Testing2
+{
+    [TestClass]
+    public class tstCustomer
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            clsCustomer AnCustomer = new clsCustomer();
+            Assert.IsNotNull(AnCustomer);
+        }
+    }
+}

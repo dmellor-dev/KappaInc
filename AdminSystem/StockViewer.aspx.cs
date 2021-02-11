55afa51981b5d60b1c4acaa2a24a605f888781b5
P2532984 <P2532984@GWHXX682WIN019W>
2021-02-11 16:39:42 +0000

    Added the StockDataEntry and StockViewer presentation layer files - week 18 work

--- AdminSystem/StockViewer.aspx.cs
+++ AdminSystem/StockViewer.aspx.cs
@@ -4,11 +4,24 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using ClassLibrary;
 
 public partial class _1Viewer : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
+        //create a new instance of clsStock
+        clsStock someStock = new clsStock();
+        //get the data from the session object
+        someStock = (clsStock)Session["someStock"];
+        //display the Item Name for this entry
+        Response.Write(someStock.ItemName);
+        Response.Write(someStock.ItemType);
+        Response.Write(someStock.StockQuantity);
+        Response.Write(someStock.Price);
+        Response.Write(someStock.Available);
+        Response.Write(someStock.Supplier);
+        Response.Write(someStock.NextRestock);
 
     }
 }

b82a023b6f5862c0ee0c6c13f16716fccb60cf1d
Kacper Bziuk <kacper.bziuk.99@gmail.com>
2021-02-19 19:27:54 +0000

    Testing for find method and hardcoded solution

--- ClassLibrary/clsStaff.cs
+++ ClassLibrary/clsStaff.cs
@@ -4,12 +4,95 @@ namespace ClassLibrary
 {
     public class clsStaff
     {
-        public bool IsWorking { get; set; }
-        public DateTime StartDate { get; set; }
-        public int StaffId { get; set; }
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
-        public string EmailAddress { get; set; }
-        public string HomeAddress { get; set; }
+        private Int32 mStaffId;
+        private DateTime mStartDate;
+        private string mFirstName;
+        private string mLastName;
+        private string mEmailAddress;
+        private string mHomeAddress;
+        private bool mIsWorking;
+
+        public bool IsWorking {
+            get
+            {
+                return mIsWorking;
+            }
+            set
+            {
+                mIsWorking = value;
+            }
+        }
+        public DateTime StartDate {
+            get
+            {
+                return mStartDate;
+            }
+            set
+            {
+                mStartDate = value;
+            }
+        }
+        public int StaffId {
+            get
+            {
+                return mStaffId;
+            }
+            set
+            {
+                mStaffId = value;
+            }
+        }
+        public string FirstName {
+            get
+            {
+                return mFirstName;
+            }
+            set
+            {
+                mFirstName = value;
+            }
+        }
+        public string LastName {
+            get
+            {
+                return mLastName;
+            }
+            set
+            {
+                mLastName = value;
+            }
+        }
+        public string EmailAddress {
+            get
+            {
+                return mEmailAddress;
+            }
+            set
+            {
+                mEmailAddress = value;
+            }
+        }
+        public string HomeAddress {
+            get
+            {
+                return mHomeAddress;
+            }
+            set
+            {
+                mHomeAddress = value;
+            }
+        }
+
+        public bool Find(int staffId)
+        {
+            mStaffId = 111;
+            mStartDate = Convert.ToDateTime("19/02/2021");
+            mFirstName = "Kacper";
+            mLastName = "Bziuk";
+            mEmailAddress = "kb@mail.com";
+            mHomeAddress = "123 Some Ln, LE2 1AB";
+            mIsWorking = true;
+            return true;
+        }
     }
 }

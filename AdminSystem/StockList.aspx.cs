08ea764a3bf67bd9e62866aece276d1cbc2fe02d
P2532984 <P2532984@GWHXX682WIN019W>
2021-03-29 17:20:25 +0100

    THE CODE LIES, DON'T CHANGE _1_LIST TO _DEFAULT... Anyway that works now

--- AdminSystem/StockList.aspx.cs
+++ AdminSystem/StockList.aspx.cs
@@ -4,11 +4,32 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using ClassLibrary;
 
 public partial class _1_List : System.Web.UI.Page
 {
+    //this function handles the load event for the page
     protected void Page_Load(object sender, EventArgs e)
     {
+        //if this is the first time the page is displayed
+        if(IsPostBack == false)
+        {
+            //update the list box
+            DisplayStock();
+        }
+    }
 
+    void DisplayStock()
+    {
+        //create an instance of the Item Collection
+        clsItemCollection AllItems = new clsItemCollection();
+        //set the data source to the list of items in the collection
+        lstStockList.DataSource = AllItems.ItemList;
+        //set the name of the primary key
+        lstStockList.DataValueField = "ItemID";
+        //set the data field to display
+        lstStockList.DataTextField = "ItemName";
+        //bind the data to the list
+        lstStockList.DataBind();
     }
 }
